85b767b1390fad46125b538de52ba185ebd2995f
xenon_d62 <xannrlcs@gmail.com>
2026-05-13 14:58:06 +0700

    Create page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,46 @@
+export default function Home() {
+  return (
+    <main className="hero">
+
+      <div className="overlay"></div>
+
+      <nav className="navbar">
+
+        <h1>NEURO SPIN</h1>
+
+        <button>
+          Connect
+        </button>
+
+      </nav>
+
+      <section className="hero-content">
+
+        <h2>
+          ENTER THE
+          <br />
+          NEUROVERSE
+        </h2>
+
+        <p>
+          Cyberpunk NFT spin mini app
+          built for Farcaster.
+        </p>
+
+        <div className="buttons">
+
+          <button>
+            Launch Spin
+          </button>
+
+          <button className="secondary">
+            Marketplace
+          </button>
+
+        </div>
+
+      </section>
+
+    </main>
+  )
+}
